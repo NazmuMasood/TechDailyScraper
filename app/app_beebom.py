70c94e10b8bd339217f3a7a8a3a61de3ccdbbb01
Nazmu Masood <nazmumasood96@gmail.com>
2021-06-19 06:56:43 +0600

    Added check for duplicated data

--- app/app_beebom.py
+++ app/app_beebom.py
@@ -1,6 +1,6 @@
-from re import T
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, select
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql.expression import null
 from sqlalchemy.sql.schema import Column, ForeignKey, Sequence
 from sqlalchemy.sql.sqltypes import DateTime, Integer, String
 from sqlalchemy.sql import func
@@ -41,7 +41,7 @@ class Content(Base):
     updated_at = Column(DateTime(timezone=True), nullable=True, onupdate=func.now())
     
     def __repr__(self):
-        return "<Content(id='%d', owner_id='%d', url='%s', title='%s', author='%s', pub_date='%s', img_url='%s', created_at='%s', updated_at='%s')>" % ( 
+        return "<Content(id='%s', owner_id='%s', url='%s', title='%s', author='%s', pub_date='%s', img_url='%s', created_at='%s', updated_at='%s')>" % ( 
             self.id, self.owner_id, self.url, self.title, self.author, self.pub_date, self.img_url, self.created_at, self.updated_at)
 
 ### Db connection
@@ -54,10 +54,30 @@ session = Session()
 
 ### ----- session code for that particular session goes here -----
 
+statement = 'SELECT techdaily_content.url FROM techdaily_content WHERE owner_id = 2 ORDER BY id DESC LIMIT 1'
+results = session.execute(statement).scalars().all()
+beebom_recent_url_in_db = 'null'
+if len(results)>0:
+    beebom_recent_url_in_db = results[0]
+print('---------Beebom last record : '+beebom_recent_url_in_db)
+# statement = select(Content.url).filter_by(owner_id=2)
+# results = session.execute(statement).scalars().all()
+# for result in results:
+#     print(result)
+#     print('\n')
+# print('list length: '+str(len(results)))
+
 # --------!!!!!------ Populating techdaily_content table -------!!!!!!!!!--------
 owner_names = ['Cnet','Beebom', 'Android Authority']
 owner_urls = ['https://www.cnet.com', 'https://beebom.com', 'https://www.androidauthority.com']
 owner_ids = [1, 2, 3]
+
+content_titles = []
+content_urls = []
+content_authors = []
+content_img_urls = []
+content_pub_dates = []
+
 beebom = 1 #choosing beebom
 owner_id = owner_ids[beebom] 
 html_text = requests.get(owner_urls[beebom]).text
@@ -67,17 +87,34 @@ for title in titles:
     contentTitle = title.find('div', class_ = 'item-details')
     aHref = contentTitle.find('a')
     image = title.find('img')
+    if (aHref['href']==beebom_recent_url_in_db):
+        print('------------MATCHING RECORD FOUND')
+        break
     print(owner_id)                #owner_id
     print(aHref.text)              #content title
     print(aHref['href'])           #content link
     print(image['src']+'\n')       #content image url
+    content_titles.append(aHref.text)
+    content_urls.append(aHref['href'])
+    content_img_urls.append(image['src'])
+    # content = Content()
+    # content.owner_id = owner_id
+    # content.title = aHref.text
+    # # content.author = 'John Doe'
+    # content.url = aHref['href']
+    # content.img_url = image['src']
+    # # content.pub_date = 'June 17, 2021'
+    # session.add(content)
+
+for content_title, content_url, content_img_url in zip(
+        reversed(content_titles), reversed(content_urls), reversed(content_img_urls)):
     content = Content()
     content.owner_id = owner_id
-    content.title = aHref.text
+    content.title = content_title
     # content.author = 'John Doe'
-    content.url = aHref['href']
-    content.img_url = image['src']
-    # content.pub_date = 'June 17, 2021'
+    content.url = content_url
+    content.img_url = content_img_url
+    # content.pub_date = content_pub_date
     session.add(content)
 
 session.commit()
--- app/app_cnet.py
+++ app/app_cnet.py
@@ -53,11 +53,24 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 ### ----- session code for that particular session goes here -----
+statement = 'SELECT techdaily_content.url FROM techdaily_content WHERE owner_id = 1 ORDER BY id DESC LIMIT 1'
+results = session.execute(statement).scalars().all()
+cnet_recent_url_in_db = 'null'
+if len(results)>0:
+    cnet_recent_url_in_db = results[0]
+print('---------Cnet last record : '+cnet_recent_url_in_db)
 
 # --------!!!!!------ Populating techdaily_content table -------!!!!!!!!!--------
 owner_names = ['Cnet','Beebom', 'Android Authority']
 owner_urls = ['https://www.cnet.com', 'https://beebom.com', 'https://www.androidauthority.com']
 owner_ids = [1, 2, 3]
+
+content_titles = []
+content_urls = []
+content_authors = []
+content_img_urls = []
+content_pub_dates = []
+
 cnet = 0 #choosing cnet
 owner_id = owner_ids[cnet] 
 html_text = requests.get(owner_urls[cnet]).text
@@ -66,18 +79,35 @@ titles = soup.find_all('div',class_ = 'row item')
 for title in titles:
     aHref = title.find('a')
     image = title.find('img')
+    if ((owner_urls[cnet]+aHref['href'])==cnet_recent_url_in_db):
+        print('------------MATCHING RECORD FOUND')
+        break
     print(owner_id)                        #owner_id
     print(aHref.text)                      #content title
     print(owner_urls[cnet]+aHref['href'])  #content url
     print(image['src']+'\n')               #content image url
+    content_titles.append(aHref.text)
+    content_urls.append(owner_urls[cnet]+aHref['href'])
+    content_img_urls.append(image['src'])
+    # content = Content()
+    # content.owner_id = owner_id
+    # content.title = aHref.text
+    # # content.author = 'John Doe'
+    # content.url = owner_urls[cnet]+aHref['href']
+    # content.img_url = image['src']
+    # # content.pub_date = 'June 17, 2021'
+    # session.add(content)
+
+for content_title, content_url, content_img_url in zip(
+        reversed(content_titles), reversed(content_urls), reversed(content_img_urls)):
     content = Content()
     content.owner_id = owner_id
-    content.title = aHref.text
+    content.title = content_title
     # content.author = 'John Doe'
-    content.url = owner_urls[cnet]+aHref['href']
-    content.img_url = image['src']
-    # content.pub_date = 'June 17, 2021'
-    session.add(content)
+    content.url = content_url
+    content.img_url = content_img_url
+    # content.pub_date = content_pub_date
+    session.add(content)    
 
 session.commit()
 session.close()
